7cf54409f92ec4ae2f379d3689de1e1429c602f5
Keith Thomas <keithxemi@users.noreply.github.com>
2022-01-02 15:04:50 -0500

    Update background.js
    Fix extraHeaders for Firefox

--- background.js
+++ background.js
@@ -1,19 +1,15 @@
-const options = [
-  chrome.webRequest.OnHeadersReceivedOptions.BLOCKING,
-  chrome.webRequest.OnHeadersReceivedOptions.RESPONSE_HEADERS,
-]
+var options = ['responseHeaders','blocking']
+
 if(chrome.webRequest.OnBeforeSendHeadersOptions.hasOwnProperty('EXTRA_HEADERS')) {
-  options.push(chrome.webRequest.OnHeadersReceivedOptions.EXTRA_HEADERS)
+  options.push('extraHeaders')
 }
 
-const getHeaderModifier = () => details => {
-  for (const responseHeader of details.responseHeaders) {
+chrome.webRequest.onHeadersReceived.addListener((details) => {
+    for (const responseHeader of details.responseHeaders) {
     if (responseHeader.name === 'Content-Type' &&
         responseHeader.value === 'text/html; charset=ISO-8859-1') {
       responseHeader.value = 'text/html; charset=utf-8'
     }
   }
-  return { responseHeaders: details.responseHeaders }
-}
-
-chrome.webRequest.onHeadersReceived.addListener(getHeaderModifier(), { urls: ["https://user.well.com/engaged.cgi*"]}, options)
+  return {responseHeaders: details.responseHeaders}
+}, {urls: ["https://user.well.com/engaged.cgi*"]}, options)
